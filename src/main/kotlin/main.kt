3297b92b49094e510ed874b6d3eb2708744cb743
Sebastien REMY <seb.remy@me.com>
2021-04-17 15:05:05 +0200

    Pairs and triples

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val fullName = Pair<String, String>("Seb", "REMY")
+    val age = 50
+
+    println ("My name is ${fullName.first}, ${fullName.second}")
+
+    val (myName: String, myLastName: String) = fullName
+    println ("My name is $myName, $myLastName")
+
+    val birthday = Triple(19,4,1971)
+    val person = Pair("Seb", birthday)
+    val (name: String, bday) = person
+    val (_, month, year) = bday
+
+    println ("$name was on $year/$month")
+
 }
